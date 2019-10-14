becb948277680dc8a17a522d7f19d8b87c4c6894
nevroje <joakim.berglind@gmail.com>
2019-10-14 23:55:09 +0200

    installed firebase and sass wip firebase component

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import 
+
+const firebaseConfig = firebase.initializeApp({
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messageSenderId: '',
+  apiId: '',
+});
+
+export { firebaseConfig as firebase};
