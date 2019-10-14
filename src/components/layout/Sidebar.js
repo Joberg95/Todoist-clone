b2a8ffcab4dad73a956caf5f9f5fcf9aa238a0cc
nevroje <joakim.berglind@gmail.com>
2019-10-14 12:54:41 +0200

    wip sidebar, imported icons for selections

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React from "react";
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+} from "react-icons/fa";
 
 export const Sidebar = () => {
   return (
-    <p>sidebar</p>
-  )
-}
+    <div className="sidebar" data-testid="sidebar">
+      <ul className="sidebar__generic">
+        <li>
+          <span>
+            <FaInbox />
+          </span>
+          <span>Inbox</span>
+        </li>
+        <li>
+          <span>
+            <FaRegCalendar />
+          </span>
+          <span>Today</span>
+        </li>
+        <li>
+          <span>
+            <FaRegCalendarAlt />
+          </span>
+          <span>Next 7 days</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
