162c0c43c4d0b20846d61d4575b8f685680b274c
nevroje <joakim.berglind@gmail.com>
2019-10-16 13:12:24 +0200

    installed eslint, wip usestate for components

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   FaChevronDown,
   FaInbox,
   FaRegCalendarAlt,
   FaRegCalendar
 } from "react-icons/fa";
+import { useSelectedProjectValue } from "../../context";
+import TestRunner from "jest-runner";
 
 export const Sidebar = () => {
+  const { setSelectedProject } = useSelectedProjectValue;
+  const [active, setActive] = useState("inbox");
+  const [showProjects, setShowProjects] = useState(true);
+
   return (
     <div className="sidebar" data-testid="sidebar">
       <ul className="sidebar__generic">
